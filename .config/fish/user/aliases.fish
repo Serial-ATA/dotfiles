10f0289d6b7a88324456690774d19022f5e97656
Serial <69764315+Serial-ATA@users.noreply.github.com>
2023-01-20 22:21:48 -0500

    fish: Add alias for start languagetool server

--- .config/fish/user/aliases.fish
+++ .config/fish/user/aliases.fish
@@ -14,6 +14,7 @@ alias ffprobe="ffprobe -show_format -pretty -loglevel debug"
 alias gitdf="git --git-dir=$PROJECTS_DIR/dotfiles/ --work-tree=$HOME"
 alias grep='grep -i --color=auto'
 alias gpg='gpg2 --homedir "$XDG_DATA_HOME"/gnupg'
+alias languagetool_server='languagetool --http --port 8081 --allow-origin "*"'
 alias ls="ls -hF --color=auto --group-directories-first"
 alias mkdir="mkdir -p"
 alias mpd="mpd $XDG_CONFIG_HOME/mpd/mpd.conf"
@@ -22,7 +23,7 @@ alias mvn='mvn -gs "$XDG_CONFIG_HOME"/maven/settings.xml'
 alias nvidia-settings='nvidia-settings --config="~/.config/nvidia/settings"'
 alias p="sudo pacman"
 alias ps="ps -u $USER"
-alias scc="scc --exclude-dir target,.idea,.git"
+alias scc="scc --exclude-dir target,.idea,.git,.venv,.env"
 alias sound="yt-dlp --ignore-config -x --audio-format mp3 -o '~/Documents/sounds/%(title)s.%(ext)s'"
 alias song="vidl opus --no-embed --no-smart-md"
 alias ss='sudo systemctl'
